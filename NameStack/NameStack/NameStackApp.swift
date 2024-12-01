2b670c5167dafc1aaa73e2c637e2f517a06ee836
이주헌 <leejooheon@ijuheon-ui-MacBookPro.local>
2024-12-01 20:27:05 +0900

    FEAT connect database to app

--- NameStack/NameStack/NameStackApp.swift
+++ NameStack/NameStack/NameStackApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import SwiftData
+
 
 @main
 struct NameStackApp: App {
+    var nameStackModelContainer: ModelContainer = {
+        let schema = Schema([Card.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true) //개발 과정에서는 isStoredInMemoryOnly를 true로
+        do {
+          return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+          fatalError("Could not create ModelContainer: \(error)")
+        }
+      }()
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(nameStackModelContainer)
     }
 }
